bc9efbb953bd8b270706540b41b48549dbe98b7b
Mu <962475158@qq.com>
2022-03-22 14:07:18 +0800

    master

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -2,13 +2,20 @@
   import { formDate } from '@/utils/time';
   import { ref, Ref } from 'vue';
   import AppIcon from '@/components/AppIcon.vue';
+  import { useRouter } from 'vue-router';
   let time: Ref<string> = ref(formDate(new Date().toISOString()));
+  const router = useRouter();
+  function goLogin() {
+    router.push({ name: 'Login' });
+  }
 </script>
 
 <template>
   <div class="layout">
     <img src="@/assets/images/head.jpg" alt="" />
-    <a href=""><app-icon icon="mdi:github" class="text-5xl cursor-pointer"></app-icon></a>
+    <a href="https://github.com/evilsfdd/MyTemplateByVite">
+      <app-icon icon="mdi:github" class="text-5xl cursor-pointer"></app-icon>
+    </a>
     <span class="text-green-100">给个star呗</span>
     <h2>
       {{ time }}
@@ -19,6 +26,7 @@
     <div>生态：vue-router路由+pinia仓库</div>
     <div>TailWind CSS框架+ElementsUI-plus组件库+Iconfy图标库</div>
     <div>工具类（待增加）：dayjs+lodash</div>
+    <button @click="goLogin">登录页</button>
   </div>
 </template>
 
@@ -37,5 +45,11 @@
   }
   div {
     font-size: 20px;
+  }
+  button {
+    width: 100px;
+    height: 40px;
+    background-color: #a2d2;
+    border-radius: 30px;
   }
 </style>
